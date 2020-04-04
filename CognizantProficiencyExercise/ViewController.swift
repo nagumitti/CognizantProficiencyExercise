12ac3da502871f7052089fcb73c8e5e750c02170
Nagaraj <nagaraj@synchronoss.com>
2020-04-04 15:15:57 +0530

    Initial Commit

---
+++ CognizantProficiencyExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CognizantProficiencyExercise
+//
+//  Created by Nagaraj on 04/04/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
