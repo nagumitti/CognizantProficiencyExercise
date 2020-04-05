06c04d79fae92cbb6577dc0e8ecc4bb21dcf7807
Nagaraj <nagaraj@synchronoss.com>
2020-04-05 10:18:20 +0530

    Added parsing of Data received from the RequestService.

--- CognizantProficiencyExercise/Models/FactsModel.swift
+++ CognizantProficiencyExercise/Models/FactsModel.swift
@@ -5,7 +5,20 @@
 //  Created by Nagaraj on 04/04/20.
 //
 
+import Foundation
+
 struct FactsModel: Decodable {
     let title: String?
     let rows: [FactDataModel]?
+}
+
+extension FactsModel: Parceable {
+  static func parseObject(data: Data) -> Result<FactsModel, ErrorResult> {
+    let decoder = JSONDecoder()
+    if let result = try? decoder.decode(FactsModel.self, from: data) {
+        return Result.success(result)
+    } else {
+        return Result.failure(ErrorResult.parser(string: "Unable to parse FactsModel results"))
+    }
+  }
 }
---
+++ CognizantProficiencyExercise/Services/Parser/ParserHelper.swift
@@ -0,0 +1,30 @@
+//
+//  ParserHelper.swift
+//  CognizantProficiencyExercise
+//
+//  Created by Nagaraj on 05/04/20.
+//
+
+import Foundation
+
+protocol Parceable {
+  static func parseObject(data: Data) -> Result<Self, ErrorResult>
+}
+
+final class ParserHelper {
+  static func parse<T: Parceable>(data: Data, completion: (Result<T, ErrorResult>) -> Void) {
+    if let response = String(data: data, encoding: String.Encoding.ascii),
+      let data = response.data(using: String.Encoding.utf8) {
+      switch T.parseObject(data: data) {
+      case .failure(let error):
+        completion(.failure(error))
+        break
+      case .success(let newModel):
+        completion(.success(newModel))
+        break
+      }
+    } else {
+      completion(.failure(.parser(string: "Error while parsing json data")))
+    }
+  }
+}
